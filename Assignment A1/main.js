f1bc74f638c29cd3ad1038a94e868d5d74e8d4c3
KiaronGovender <kiagov02@gmail.com>
2026-07-16 11:34:35 +0200

    Stage 0: hello server

--- Assignment A1/main.js
+++ Assignment A1/main.js
@@ -1,11 +1,12 @@
 const express = require("express");
 
 const app = express();
+const PORT = 3000;
 
 app.get("/", (req, res) => {
   res.send("hello World!");
 });
 
-app.listen(3000, () => {
-  console.log("Listening on port 3000");
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
 });
