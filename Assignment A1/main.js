61dfc9bd3982c5a6816e6d6391beb02a9a15ec35
KiaronGovender <kiagov02@gmail.com>
2026-07-16 11:57:19 +0200

    Stage 2: read endpoints with 404

--- Assignment A1/main.js
+++ Assignment A1/main.js
@@ -3,6 +3,24 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
+let tasks = [
+  {
+    id: 1,
+    title: "Hello, Server!",
+    done: true,
+  },
+  {
+    id: 2,
+    title: "Your first real endpoint!",
+    done: true,
+  },
+  {
+    id: 3,
+    title: "Read: List and single task",
+    done: true,
+  },
+];
+
 app.get("/", (req, res) => {
   return res.json({
     name: "Task API",
@@ -13,6 +31,22 @@ app.get("/", (req, res) => {
 
 app.get("/health", (req, res) => {
   return res.json({ status: "ok" });
+});
+
+app.get("/tasks", (req, res) => {
+  return res.json(tasks);
+});
+
+app.get("/tasks/:id", (req, res) => {
+  const id = Number(req.params.id);
+
+  const task = tasks.find((task) => task.id === id);
+
+  if (!task) {
+    return res.status(404).json({ error: `Task ${id} not found` });
+  }
+
+  return res.status(200).json(task);
 });
 
 app.listen(PORT, () => {
