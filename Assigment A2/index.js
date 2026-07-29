c43d2520895179b2683d440f6f6735ed241c90a0
KiaronGovender <kiagov02@gmail.com>
2026-07-29 15:00:39 +0200

    Stage 1: database read endpoints

--- Assigment A2/index.js
+++ Assigment A2/index.js
@@ -60,7 +60,7 @@ app.get("/tasks", (req, res) => {
 app.get("/tasks/:id", (req, res) => {
   const id = Number(req.params.id);
 
-  const task = tasks.find((task) => task.id === id);
+  const task = db.prepare(`SELECT * FROM tasks WHERE id = ${id}`).all();
 
   if (!task) {
     return res.status(404).json({ error: `Task ${id} not found` });
