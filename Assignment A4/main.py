28fa0e6c1515d197176f63047b434e5ff3950ed9
KiaronGovender <kiagov02@gmail.com>
2026-09-14 11:26:55 +0200

    Stage 2: public route and unverified protected route

--- Assignment A4/main.py
+++ Assignment A4/main.py
@@ -37,3 +37,14 @@ def login(email:str, password:str, db: Client = Depends(get_supabase)):
             status_code=status.HTTP_400_BAD_REQUEST, 
             detail=str(e)
         )
+
+@app.get('/public/info')
+def public_info():
+    return JSONResponse(status_code=200,content={"message": "Welcome stranger! This info is public"})
+
+@app.get('/protected/profile')
+def protected_profile(access_token):
+    if not access_token:
+        return JSONResponse(status_code=401, content={"error":"missing Access Token required"})
+
+    return {"Access Token":access_token}
